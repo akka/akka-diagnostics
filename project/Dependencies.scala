37c096eb78c34ba7312917f9afec36d0150fcee4
Francisco Lopez-Sancho <franciscolopezsancho@gmail.com>
2023-02-03 15:23:16 +0100

    bug: Dealing with persistence testkit plugin configuration (#32)
    * Ignoring config from PersistenceTestKitPlugin.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,12 +20,14 @@ object Dependencies {
     val akkaRemoting = "com.typesafe.akka" %% "akka-remote" % AkkaVersion
     val akkaClusterMetrics = "com.typesafe.akka" %% "akka-cluster-metrics" % AkkaVersion
     val akkaStreamTestKit = "com.typesafe.akka" %% "akka-stream-testkit" % AkkaVersion
+    val akkaPersistenceTestKit = "com.typesafe.akka" %% "akka-persistence-testkit" % AkkaVersion
     val scalaTest = "org.scalatest" %% "scalatest" % "3.0.8"
     val junit = "junit" % "junit" % "4.13.2" // Common Public License 1.0
     val all = Seq(
       akkaRemoting % Test,
       akkaClusterMetrics % Test,
       akkaStreamTestKit % Test,
+      akkaPersistenceTestKit % Test,
       scalaTest % Test, // ApacheV2
       junit % Test // Common Public License 1.0
     )
